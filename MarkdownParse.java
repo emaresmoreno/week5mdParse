e4df515bae2858eb1a7761acb2931e863030b0ab
emaresmoreno <emaresmoreno@ucsd.edu>
2022-04-29 13:41:43 -0700

    Purposeful Error
    MarkdownParse.java:44: error: incompatible types: String cannot be converted to Path
            String content = Files.readString(fileName + "1");
                                                       ^
    Note: Some messages have been simplified; recompile with -Xdiags:verbose to get full output
    1 error

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -41,7 +41,7 @@ public class MarkdownParse {
 
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
-        String content = Files.readString(fileName + "3");
+        String content = Files.readString(fileName + "1");
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
     }
